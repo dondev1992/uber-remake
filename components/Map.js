4f6678d56e9892e61ea2430842c033b3f3da6fca
dondev1992 <donsosa1@gmail.com>
2021-08-09 21:48:55 -0400

    Second Commit

---
+++ components/Map.js
@@ -0,0 +1,77 @@
+import React, { useRef, useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import { useSelector } from "react-redux";
+import { selectDestination, selectOrigin } from "../slices/navSlice";
+import tw from "tailwind-react-native-classnames";
+import MapViewDirections from "react-native-maps-directions";
+import { GOOGLE_MAPS_API_KEY } from "@env";
+
+const Map = () => {
+  const origin = useSelector(selectOrigin);
+  const destination = useSelector(selectDestination);
+  const mapRef = useRef(null);
+
+  useEffect(() => {
+    if (!origin || !destination) return;
+    // Zoom and fit to markers
+    mapRef.current.fitToSuppliedMarkers(["origin", "destination"], {
+      edgePadding: { top: 50, right: 50, bottom: 50, left: 50 },
+    });
+  }, [origin, destination]);
+
+  return (
+    <MapView
+      ref={mapRef}
+      style={tw`flex-1`}
+      mapType="mutedStandard"
+      initialRegion={{
+        latitude: origin.location.lat,
+        longitude: origin.location.lng,
+        latitudeDelta: 0.005,
+        longitudeDelta: 0.005,
+      }}
+    >
+      {origin?.location && (
+        <Marker
+          coordinate={{
+            latitude: origin.location.lat,
+            longitude: origin.location.lng,
+          }}
+          title="Origin"
+          description={origin.description}
+          identifier="origin"
+        />
+      )}
+      {destination?.location && (
+        <Marker
+          coordinate={{
+            latitude: destination.location.lat,
+            longitude: destination.location.lng,
+          }}
+          title="Destination"
+          description={destination.description}
+          identifier="destination"
+        />
+      )}
+      {origin && destination && (
+        <MapViewDirections
+          origin={origin.description}
+          lineDashPattern={[0]}
+          destination={destination.description}
+          apikey={GOOGLE_MAPS_API_KEY}
+          strokeColor="black"
+          strokeWidth={3}
+        />
+      )}
+    </MapView>
+  );
+};
+
+export default Map;
+
+const styles = StyleSheet.create({
+  map: {
+    height: 400,
+  },
+});
